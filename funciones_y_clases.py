1ea260803e2cec135fde128672db362a993f87f0
marion fonseca <rossfonseca@gmail.com>
2021-02-27 14:09:04 +0000

    .

--- funciones_y_clases.py
+++ funciones_y_clases.py
@@ -1,12 +1,17 @@
 global1 = 34
 
-def cambiar_global():
+def cambiar_global(a):
     '''Cambiar una variable global
 
     Esta función debe asignarle a la variable global `global1` el valor que se
     le pasa como único argumento posicional.
     '''
-    pass
+    global global1
+    global1 = a
+    print (global1)
+
+cambiar_global(7)
+    
 
 
 def anio_bisiesto():
